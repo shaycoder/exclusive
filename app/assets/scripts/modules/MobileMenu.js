706c15e135e6eca60aa64183211219052aa8516c
Nitin Rajan <nitin.rajan15@gmail.com>
2023-11-02 13:06:34 +0530

    Completed front page for content

--- app/assets/scripts/modules/MobileMenu.js
+++ app/assets/scripts/modules/MobileMenu.js
@@ -1,6 +1,7 @@
 class MobileMenu {
     constructor() {
         this.menuIcon = document.querySelector('.site-header__menu-icon')
+        this.menuContent = document.querySelector('.site-header__nav')
         this.menuHeader = document.querySelector('.site-header')
         this.menuElement = document.querySelectorAll('.menu-element')
         this.events()
@@ -8,20 +9,10 @@ class MobileMenu {
 
     events() {
         this.menuIcon.addEventListener("click", () => this.toggleTheMenu())
-        this.menuElement.forEach(el => {
-            el.addEventListener('click', () => {
-              this.removeTheMenu()
-            });
-        });
-    }
-
-    removeTheMenu() {
-        this.menuHeader.classList.remove('site-header--hide')
-        this.menuHeader.classList.remove('site-header--is-visible')
-        this.menuIcon.classList.remove('site-header__menu-icon--close-x')
     }
 
     toggleTheMenu() {
+        this.menuContent.classList.toggle('site-header__nav--is-visible')
         this.menuHeader.classList.toggle('site-header--is-visible')
         this.menuIcon.classList.toggle('site-header__menu-icon--close-x')
     }
